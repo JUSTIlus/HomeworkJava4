5fb9acfd941af4bde80096d64733ee4261113c26
JUSTIlus <110375719+JUSTIlus@users.noreply.github.com>
2023-07-13 21:51:46 +0200

    Add files via upload
    HomeworkJAVA 4

---
+++ Homework4_2.java
@@ -0,0 +1,26 @@
+package task2;
+
+public class Homework4_2 {
+    public static void main(String[] args) {
+        System.out.println(add(4, 5));
+        System.out.println(minus(4, 5));
+        System.out.println(div(4, 5));
+        System.out.println(multi(4, 5));
+    }
+
+    public static float add(float a, float b) {
+        return a + b;
+    }
+
+    public static float minus(float a, float b) {
+        return a - b;
+    }
+
+    public static float div(float a, float b) {
+        return a / b;
+    }
+
+    public static float multi(float a, float b) {
+        return a * b;
+    }
+}
